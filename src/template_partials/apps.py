d51a0bc653be36b848f61ccfa05deff3af1d8828
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2023-08-01 11:45:47 +0100

    Address review comments.

--- src/template_partials/apps.py
+++ src/template_partials/apps.py
@@ -6,6 +6,26 @@ App configuration to set up a partials loader automatically.
 from django.apps import AppConfig
 from django.conf import settings
 
+def wrap_loaders(name):
+    for template_config in settings.TEMPLATES:
+        engine_name = template_config.get('NAME')
+        if not engine_name:
+            engine_name = template_config["BACKEND"].split(".")[-2]
+        if engine_name == name:
+            loaders = template_config.setdefault("OPTIONS", {}).get("loaders", [])
+            already_configured = (loaders and isinstance(loaders, (list, tuple)) and
+                                  isinstance(loaders[0], tuple) and
+                                  loaders[0][0] == "template_partials.loader.Loader")
+            if not already_configured:
+                template_config.pop("APP_DIRS", None)
+                default_loaders = [
+                    "django.template.loaders.filesystem.Loader",
+                    "django.template.loaders.app_directories.Loader",
+                ]
+                cached_loaders = [("django.template.loaders.cached.Loader", default_loaders)]
+                partial_loaders = [("template_partials.loader.Loader", cached_loaders)]
+                template_config['OPTIONS']['loaders'] = partial_loaders
+            break
 
 class LoaderAppConfig(AppConfig):
     """
@@ -15,27 +35,12 @@ class LoaderAppConfig(AppConfig):
     default = True
 
     def ready(self):
-        for template_config in settings.TEMPLATES:
-            if template_config["BACKEND"] == "django.template.backends.django.DjangoTemplates":
-                loaders = template_config.setdefault("OPTIONS", {}).get("loaders", [])
-                already_configured = (loaders and isinstance(loaders, (list, tuple)) and
-                                      isinstance(loaders[0], tuple) and
-                                      loaders[0][0] == "template_partials.loader.Loader")
-                if not already_configured:
-                    template_config.pop("APP_DIRS", None)
-                    default_loaders = [
-                        "django.template.loaders.filesystem.Loader",
-                        "django.template.loaders.app_directories.Loader",
-                    ]
-                    cached_loaders = [("django.template.loaders.cached.Loader", default_loaders)]
-                    partial_loaders = [("template_partials.loader.Loader", cached_loaders)]
-                    template_config['OPTIONS']['loaders'] = partial_loaders
-                break
+        wrap_loaders("django")
 
 
-class BaseAppConfig(AppConfig):
+class SimpleAppConfig(AppConfig):
     """
     This, the non-default configuration, allows the user to opt-out of the automatic configuration. They just need to
-    add "template_partials.apps.BaseAppConfig" to INSTALLED_APPS instead of "template_partials".
+    add "template_partials.apps.SimpleAppConfig" to INSTALLED_APPS instead of "template_partials".
     """
     name = "template_partials"
