carltongibson/django-template-partials

commit 88e60b125510a485fe5f4528e0fd3c6173bd2971

Adjusted to use Parser.extra_data in Django 5.1. (#23)

src/template_partials/loader.py
@@ -39,11 +39,17 @@ class Loader(BaseLoader):
         if not partial_name:
             return template
 
+        # From Django 5.1, the template has the extra_data attribute.
+        # Use that, falling back to reference on the origin.
         try:
-            partial_contents = template.origin.partial_contents
+            extra_data = getattr(template, "extra_data")
+            partial_contents = extra_data.get("template-partials", {})
         except AttributeError:
-            # No partials defined on this template.
-            raise TemplateDoesNotExist(partial_name, tried=[template_name])
+            try:
+                partial_contents = template.origin.partial_contents
+            except AttributeError:
+                # No partials defined on this template.
+                raise TemplateDoesNotExist(partial_name, tried=[template_name])
         try:
             partial = partial_contents[partial_name]
         except KeyError:

src/template_partials/templatetags/partials.py
@@ -50,17 +50,11 @@ class DefinePartialNode(template.Node):
 
 
 class RenderPartialNode(template.Node):
-    def __init__(self, partial_name, origin):
-        self.partial_name = partial_name
-        self.origin = origin
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
 
     def render(self, context):
-        """Render the partial content from the context"""
-        # Use the origin to get the partial content, because it's per Template,
-        # and available to the Parser.
-        # TODO: raise a better error here.
-        nodelist = self.origin.partial_contents[self.partial_name]
-        return nodelist.render(context)
+        return self.nodelist.render(context)
 
 
 @register.tag(name="startpartial")
@@ -104,11 +98,19 @@ def startpartial_func(parser, token):
     nodelist = parser.parse(("endpartial",))
     parser.delete_first_token()
 
-    if not hasattr(parser.origin, "partial_contents"):
-        parser.origin.partial_contents = {}
-    parser.origin.partial_contents[partial_name] = TemplateProxy(
-        nodelist, parser.origin, partial_name
-    )
+    # Store the partial nodelist in the parser.extra_data attribute, if available. (Django 5.1+)
+    # Otherwise, store it on the origin.
+    if hasattr(parser, "extra_data"):
+        parser.extra_data.setdefault("template-partials", {})
+        parser.extra_data["template-partials"][partial_name] = TemplateProxy(
+            nodelist, parser.origin, partial_name
+        )
+    else:
+        if not hasattr(parser.origin, "partial_contents"):
+            parser.origin.partial_contents = {}
+        parser.origin.partial_contents[partial_name] = TemplateProxy(
+            nodelist, parser.origin, partial_name
+        )
 
     return DefinePartialNode(partial_name, inline, nodelist)
 
@@ -131,4 +133,11 @@ def partial_func(parser, token):
             "%r tag requires a single argument" % token.contents.split()[0]
         )
 
-    return RenderPartialNode(partial_name, origin=parser.origin)
+    try:
+        extra_data = getattr(parser, "extra_data")
+        partial_contents = extra_data.get("template-partials", {})
+    except AttributeError:
+        partial_contents = parser.origin.partial_contents
+
+    nodelist = partial_contents[partial_name]
+    return RenderPartialNode(nodelist)

.github/workflows/package-tests.yml
@@ -7,28 +7,45 @@ on:
   pull_request:
 
 jobs:
-  build:
+  coverage:
     runs-on: ubuntu-latest
     strategy:
       fail-fast: false
       matrix:
         python-version:
-          - "3.11"
-
+          - "3.12"
     steps:
-      - uses: actions/checkout@v3
-
+      - uses: actions/checkout@v4
       - name: Set up Python ${{ matrix.python-version }}
         uses: actions/setup-python@v4
         with:
           python-version: ${{ matrix.python-version }}
-
       - name: Install dependencies
         run: |
           python -m pip install -e .[tests]
-
       - name: Run tests with coverage
         run: |
           coverage erase
           coverage run -m django test --settings=tests.settings --pythonpath=.
           coverage report
+  tox:
+    runs-on: ubuntu-latest
+    strategy:
+      fail-fast: false
+      matrix:
+        python-version:
+          - "3.10"
+          - "3.11"
+          - "3.12"
+    steps:
+      - uses: actions/checkout@v4
+      - name: Set up Python ${{ matrix.python-version }}
+        uses: actions/setup-python@v4
+        with:
+          python-version: ${{ matrix.python-version }}
+      - name: Install dependencies
+        run: |
+          python -m pip install --upgrade pip setuptools wheel
+          python -m pip install --upgrade 'tox>=4.0.0rc3'
+      - name: Run tox targets for ${{ matrix.python-version }}
+        run: tox run -f py$(echo ${{ matrix.python-version }} | tr -d .)

.gitignore
@@ -2,3 +2,4 @@ dist
 *.pyc
 
 .idea
+.tox

tox.ini
@@ -1,0 +1,17 @@
+[tox]
+env_list =
+    py312-django{main, 50, 42}
+    py311-django
+    py310-django
+
+[testenv]
+package = wheel
+deps =
+    django: Django
+    django42: Django>=4.2,<5.0
+    django50: Django>=5.0a1,<6.0
+    djangomain: https://github.com/django/django/archive/main.tar.gz
+set_env =
+    PYTHONDEVMODE = 1
+commands =
+    django-admin test --settings=tests.settings --pythonpath=.

commit 3765018f9d9403a18636c6cbdb4006bb86d7b1fb

Rollback pre-5.1 compat changes.
Regression in 88e60b125510a485fe5f4528e0fd3c6173bd2971.

Without adding `get_nodes_by_type()` to TemplateProxy, an error is
introduced loading `extends` tags.

Possibly:

* Caused by timing change in RenderPartialNode.
* Adding get_nodes_by_type() is correct. But tests demonstrating this
  would be needed.

Reverting these changes, and disabling tox run on CI to investigate at
leisure.

src/template_partials/loader.py
@@ -39,17 +39,11 @@ class Loader(BaseLoader):
         if not partial_name:
             return template
 
-        # From Django 5.1, the template has the extra_data attribute.
-        # Use that, falling back to reference on the origin.
         try:
-            extra_data = getattr(template, "extra_data")
-            partial_contents = extra_data.get("template-partials", {})
+            partial_contents = template.origin.partial_contents
         except AttributeError:
-            try:
-                partial_contents = template.origin.partial_contents
-            except AttributeError:
-                # No partials defined on this template.
-                raise TemplateDoesNotExist(partial_name, tried=[template_name])
+            # No partials defined on this template.
+            raise TemplateDoesNotExist(partial_name, tried=[template_name])
         try:
             partial = partial_contents[partial_name]
         except KeyError:

src/template_partials/templatetags/partials.py
@@ -52,11 +52,17 @@ class DefinePartialNode(template.Node):
 
 
 class RenderPartialNode(template.Node):
-    def __init__(self, nodelist):
-        self.nodelist = nodelist
+    def __init__(self, partial_name, origin):
+        self.partial_name = partial_name
+        self.origin = origin
 
     def render(self, context):
-        return self.nodelist.render(context)
+        """Render the partial content from the context"""
+        # Use the origin to get the partial content, because it's per Template,
+        # and available to the Parser.
+        # TODO: raise a better error here.
+        nodelist = self.origin.partial_contents[self.partial_name]
+        return nodelist.render(context)
 
 
 @register.tag(name="partialdef")
@@ -113,19 +119,11 @@ def _define_partial(parser, token, end_tag):
     nodelist = parser.parse((end_tag,))
     parser.delete_first_token()
 
-    # Store the partial nodelist in the parser.extra_data attribute, if available. (Django 5.1+)
-    # Otherwise, store it on the origin.
-    if hasattr(parser, "extra_data"):
-        parser.extra_data.setdefault("template-partials", {})
-        parser.extra_data["template-partials"][partial_name] = TemplateProxy(
-            nodelist, parser.origin, partial_name
-        )
-    else:
-        if not hasattr(parser.origin, "partial_contents"):
-            parser.origin.partial_contents = {}
-        parser.origin.partial_contents[partial_name] = TemplateProxy(
-            nodelist, parser.origin, partial_name
-        )
+    if not hasattr(parser.origin, "partial_contents"):
+        parser.origin.partial_contents = {}
+    parser.origin.partial_contents[partial_name] = TemplateProxy(
+        nodelist, parser.origin, partial_name
+    )
 
     return DefinePartialNode(partial_name, inline, nodelist)
 
@@ -148,11 +146,4 @@ def partial_func(parser, token):
             "%r tag requires a single argument" % token.contents.split()[0]
         )
 
-    try:
-        extra_data = getattr(parser, "extra_data")
-        partial_contents = extra_data.get("template-partials", {})
-    except AttributeError:
-        partial_contents = parser.origin.partial_contents
-
-    nodelist = partial_contents[partial_name]
-    return RenderPartialNode(nodelist)
+    return RenderPartialNode(partial_name, origin=parser.origin)

.github/workflows/package-tests.yml
@@ -13,7 +13,7 @@ jobs:
       fail-fast: false
       matrix:
         python-version:
-          - "3.12"
+          - "3.11"
     steps:
       - uses: actions/checkout@v4
       - name: Set up Python ${{ matrix.python-version }}
@@ -28,24 +28,24 @@ jobs:
           coverage erase
           coverage run -m django test --settings=tests.settings --pythonpath=.
           coverage report
-  tox:
-    runs-on: ubuntu-latest
-    strategy:
-      fail-fast: false
-      matrix:
-        python-version:
-          - "3.10"
-          - "3.11"
-          - "3.12"
-    steps:
-      - uses: actions/checkout@v4
-      - name: Set up Python ${{ matrix.python-version }}
-        uses: actions/setup-python@v4
-        with:
-          python-version: ${{ matrix.python-version }}
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip setuptools wheel
-          python -m pip install --upgrade 'tox>=4.0.0rc3'
-      - name: Run tox targets for ${{ matrix.python-version }}
-        run: tox run -f py$(echo ${{ matrix.python-version }} | tr -d .)
+#   tox:
+#     runs-on: ubuntu-latest
+#     strategy:
+#       fail-fast: false
+#       matrix:
+#         python-version:
+#           - "3.10"
+#           - "3.11"
+#           - "3.12"
+#     steps:
+#       - uses: actions/checkout@v4
+#       - name: Set up Python ${{ matrix.python-version }}
+#         uses: actions/setup-python@v4
+#         with:
+#           python-version: ${{ matrix.python-version }}
+#       - name: Install dependencies
+#         run: |
+#           python -m pip install --upgrade pip setuptools wheel
+#           python -m pip install --upgrade 'tox>=4.0.0rc3'
+#       - name: Run tox targets for ${{ matrix.python-version }}
+#         run: tox run -f py$(echo ${{ matrix.python-version }} | tr -d .)